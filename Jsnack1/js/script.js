a36a95e038ef9f34f24e99f3dcf4c502c3a137f7
Niccolo Delfino <delfino.nicco@libero.it>
2021-01-18 15:56:18 +0100

    Jsnack1 ok

---
+++ Jsnack1/js/script.js
@@ -0,0 +1,17 @@
+// Jsnack1 - Creare un oggetto palla che abbia le seguenti proprietà.
+// Nome = palla
+// Peso = 10
+// Attraverso un prompt dare la possibilità all'utente di modificare il peso della palla.
+
+var palla = {
+  nome : palla,
+  peso : 10
+};
+
+console.log(palla.peso);
+
+var chiediPeso = parseInt(prompt('Quanto pesa la palla?'));
+
+palla.peso = chiediPeso;
+
+console.log(palla.peso);
