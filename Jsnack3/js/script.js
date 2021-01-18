ea548f8ece9079be74e11a5f99b12012323444d9
Niccolo Delfino <delfino.nicco@libero.it>
2021-01-18 16:27:16 +0100

    Jsnack3 Init

---
+++ Jsnack3/js/script.js
@@ -0,0 +1,56 @@
+// Jsnack3 - Creare  un array che contiene 10 oggetti che rappresentano una zucchina.
+// Dividi in due array separati le zucchine che misurano meno o più di 15cm.
+// Infine stampa separatamente quanto pesano i due gruppi di zucchine.
+
+var zucchine = [
+  {
+    varieta : 'Nero di Milano',
+    peso : 50,
+    lunghezza : 20
+  },
+  {
+    varieta : 'Alberello di Sarzana',
+    peso : 100,
+    lunghezza : 10
+  },
+  {
+    varieta : 'Bolognese',
+    peso : 50,
+    lunghezza : 30
+  },
+  {
+    varieta : 'Melissa',
+    peso : 300,
+    lunghezza : 25
+  },
+  {
+    varieta : 'Ortano',
+    peso : 400,
+    lunghezza : 15
+  },
+  {
+    varieta : 'Graffiti',
+    peso : 150,
+    lunghezza : 20
+  },
+  {
+    varieta : 'Striato d′Italia',
+    peso : 250,
+    lunghezza : 30
+  },
+  {
+    varieta : 'Lungo bianco',
+    peso : 300,
+    lunghezza : 10
+  },
+  {
+    varieta : 'Lungo fiorentino',
+    peso : 350,
+    lunghezza : 25
+  },
+  {
+    varieta : 'Romanesco',
+    peso : 450,
+    lunghezza : 10
+  }
+];
